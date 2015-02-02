376c39398eb68dcd6444f8a6d574500f06e3cd64
Esa Fauzi <Ganteng Imut@Ganteng_Imut-pc>
2015-02-02 16:05:16 +0700

    Init Projek

---
+++ application/models/JenisSMasuk.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of JenisSMasuk
+ *
+ * @author Ganteng Imut
+ */
+class JenisSMasuk {
+    //put your code here
+    function __construct(){
+        parent::__construct();
+    }
+    
+    function insert($data){
+        $this->db->insert('t_jenis_surat_masuk', $data);
+    }
+    
+    function selectAll(){
+        $this->db->select('*');
+        $this->db->from('t_jenis_surat_masuk');
+        $this->db->order_by('date_modified', 'desc');
+        return $this->db->get();
+    }
+    function selectById($id){
+        $this->db->select('*');
+        $this->db->from('t_jenis_surat_masuk');
+        $this->db->where('id_jns', $id);
+        return $this->db->get();
+    }
+     
+    function update($id, $data){
+        $this->db->where('id_jns', $id);
+        $this->db->update('t_jenis_surat_masuk', $data);
+    }
+    
+    function delete($id){
+        $this->db->where('id_jns', $id);
+        $this->db->delete('t_jenis_surat_masuk');
+    }
+    
+    // function yang digunakan oleh paginationsample
+    function selectAllPaging($limit=array()){
+        $this->db->select('*');
+        $this->db->from('t_jenis_surat_masuk');
+        $this->db->order_by('date_modified', 'desc');
+        if ($limit != NULL)
+        $this->db->limit($limit['perpage'], $limit['offset']);
+        return $this->db->get();
+    }
+}
---
+++ application/models/UnitTujuan.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UnitTujuan
+ *
+ * @author Ganteng Imut
+ */
+class UnitTujuan {
+    //put your code here
+    function __construct(){
+        parent::__construct();
+    }
+    
+    function insert($data){
+        $this->db->insert('t_unit_tujuan', $data);
+    }
+    
+    function selectAll(){
+        $this->db->select('*');
+        $this->db->from('t_unit_tujuan');
+        $this->db->order_by('date_modified', 'desc');
+        return $this->db->get();
+    }
+    function selectById($id){
+        $this->db->select('*');
+        $this->db->from('t_unit_tujuan');
+        $this->db->where('id_unit', $id);
+        return $this->db->get();
+    }
+     
+    function update($id, $data){
+        $this->db->where('id_unit', $id);
+        $this->db->update('t_unit_tujuan', $data);
+    }
+    
+    function delete($id){
+        $this->db->where('id_unit', $id);
+        $this->db->delete('t_unit_tujuan');
+    }
+    
+    // function yang digunakan oleh paginationsample
+    function selectAllPaging($limit=array()){
+        $this->db->select('*');
+        $this->db->from('t_unit_tujuan');
+        $this->db->order_by('date_modified', 'desc');
+        if ($limit != NULL)
+        $this->db->limit($limit['perpage'], $limit['offset']);
+        return $this->db->get();
+    }
+}
